f65551864ab4224120d35cd2ae1db31188b58916
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-05-09 13:14:22 -0300

    P2ex001.py

---
+++ SI_IFES/python_PROG_I/P2ex001.py
@@ -0,0 +1,6 @@
+num1 = float(input('Digite um numero:'))
+num2 = float(input('Digite outro numero'))
+if num1 < num2 :
+    print('O numero %f e o maior numero'%num1)
+else :
+    print('O numero %f e o maior numero'%num2)
