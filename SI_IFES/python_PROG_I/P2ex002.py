fa8cc56953d944a3667f06b15cd3fb3d69888055
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-05-09 13:39:03 -0300

    P2ex002.py

---
+++ SI_IFES/python_PROG_I/P2ex002.py
@@ -0,0 +1,7 @@
+sexo = str(input('Qual genero voce e? M-masculino ou F-feminino?')).upper()
+if sexo == 'F' :
+    print('O seu genero e feminino')
+if sexo == 'M' :
+    print('O seu genero e masculino')
+if sexo != 'F' and sexo != 'M' :
+    print('voce e um helicoptero de combate')
