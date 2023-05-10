bc3b539ddbef4623a743e856e9587f9d846ff0f3
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-05-10 14:30:02 -0300

    Update P2ex009.py

--- SI_IFES/python_PROG_I/P2ex009.py
+++ SI_IFES/python_PROG_I/P2ex009.py
@@ -1,20 +1,23 @@
 produto = int(input('Voce deseja comprar um produto?,insira seu codigo(1 a 40)'))
-if 1 <= produto < 10 :
+if produto <= 10 :
     preço = 10.00
-if 11 <= produto < 20 :
+elif produto <= 20 :
     preço = 15.00
-if 21 <= produto < 30 :
+elif produto <= 30 :
     preço = 20.00
-if 31 <= produto < 40 :
+elif produto < 40 :
     preço = 30.00
 pergadicionar = str(input('Voce deseja adicionar outro produto?')).lower()
 if pergadicionar == 'sim' :
- adicionar = int(input('Qual seria ele?'))
- adicionar = produto + adicionar
+    adicionar = int(input('Qual seria ele?'))
+    adicionar = produto + adicionar
+elif pergadicionar == 'nao':
+    print('Entao vou calcular o desconto')
+adicionar = preço
 if adicionar <= 250 :
-   desconto = adicionar - (adicionar * (5/100))
-if 250 < produto <= 500 :
-   desconto = adicionar - (adicionar * (10/100))
+    desconto = adicionar - (adicionar * (5/100))
+elif produto <= 500 :
+    desconto = adicionar - (adicionar * (10/100))
 else :
     desconto = adicionar - (adicionar * (15/100))
 print('O valor total do produto total e %.2f e esse mesmo valor com o desconto e %.2f'%(adicionar,desconto))
