5ea288390eaf33faae5b4c7ad6374ce215697996
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-05-10 14:31:58 -0300

    Update P2ex004.py

--- SI_IFES/python_PROG_I/P2ex004.py
+++ SI_IFES/python_PROG_I/P2ex004.py
@@ -1,11 +1,11 @@
 hora = float(input('Que horas sao agora?'))
 if 0 <= hora < 5 :
     print('Hora de mimir')
-if 5 <= hora < 12 :
+elif 5 <= hora < 12 :
     print('Bom dia')
-if 12 <= hora < 18 :
+elif 12 <= hora < 18 :
     print('Boa tarde')
-if 18 <= hora < 24 :
+elif 18 <= hora < 24 :
     print('Boa noite')
-if hora >= 25 :
+else
     print('Voce quebrou o espaço tempo')
