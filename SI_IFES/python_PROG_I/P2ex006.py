2bafe1347b82afd65038506503663b8191cdfa91
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-05-10 09:00:29 -0300

    P2ex006.py

---
+++ SI_IFES/python_PROG_I/P2ex006.py
@@ -0,0 +1,11 @@
+perguntacombustivel = str(input('Qual combustivel voce usa para abasteçer, Gasulina(G)ou Alcool(A)?'))
+litros = float(input('Quantos litros voce deseja desse combustivel?'))
+if perguntacombustivel == 'A' and litros >= 20 :
+    desconto = (3.20 * litros) - (litros * (3/100))
+else :
+    desconto = (3.20 * litros) - (litros * (5/100))
+if perguntacombustivel == 'G' and litros >= 20 :
+    desconto =(3.90 * litros) - (litros * (4/100))
+else :
+    desconto =(3.90 * litros) - (litros * (6/100))
+print('O valor total que voce gastou para absteçer e %.2f'%desconto)
