5aa25c909e9af6b70d8cb7dc02e68ce1eba3a613
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-05-15 16:25:52 -0300

    Update P2ex008.py

--- SI_IFES/python_PROG_I/P2ex008.py
+++ SI_IFES/python_PROG_I/P2ex008.py
@@ -6,22 +6,22 @@ nota4 = float(input('Qual foi a nota do quarto jurado?'))
 nota5 = float(input('Qual foi a nota do quinto jurado?'))
 if (nota1 > nota2) and (nota1 > nota3) and (nota1 > nota4) and (nota1 > nota5) :
     nota_maior = nota1
-elif (nota2 > nota1) and (nota2 > nota3) and (nota2 > nota4) and (nota2 > nota5) :
+elif (nota2 > nota3) and (nota2 > nota4) and (nota2 > nota5) :
     nota_maior = nota2
-elif (nota3 > nota2) and (nota3 > nota1) and (nota3 > nota4) and (nota3 > nota5) :
+elif (nota3 > nota4) and (nota3 > nota5) :
     nota_maior = nota3
-elif (nota4 > nota2) and (nota4 > nota3) and (nota4 > nota1) and (nota4 > nota5) :
+elif(nota4 > nota5) :
     nota_maior = nota4
 else:
      nota_maior = nota5
 if (nota1 < nota2) and (nota1 < nota3) and (nota1 < nota4) and (nota1 < nota5) :
     nota_menor = nota1
-elif (nota2 < nota1) and (nota2 < nota3) and (nota2 < nota4) and (nota2 < nota5) :
+elif (nota2 < nota3) and (nota2 < nota4) and (nota2 < nota5) :
     nota_menor = nota2
-elif (nota3 < nota2) and (nota3 < nota1) and (nota3 < nota4) and (nota3 < nota5) :
+elif (nota3 < nota4) and (nota3 < nota5) :
     nota_menor = nota3
-elif (nota4 < nota2) and (nota4 < nota3) and (nota4 < nota1) and (nota4 < nota5) :
-    nota_menor = nota4
+elif (nota4 < nota5) :
+     nota_menor = nota4
 else:
      nota_menor = nota5
 media = ((nota1 + nota2 + nota3 + nota4 + nota5) - (nota_maior - nota_menor)) / 3
