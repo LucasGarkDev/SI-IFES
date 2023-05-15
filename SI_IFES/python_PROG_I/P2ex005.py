b799370eefc0726dba274597f404ae885c661564
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-05-15 15:39:30 -0300

    Update P2ex005.py

--- SI_IFES/python_PROG_I/P2ex005.py
+++ SI_IFES/python_PROG_I/P2ex005.py
@@ -15,3 +15,22 @@ elif num3 > num1 > num2 :
 else :
     print('A ordem sera %d, %d, %d'%(num3, num2, num1))
     
+    
+    
+num1 = int(input('Digite um numero:'))
+num2 = int(input('Digite um numero:'))
+num3 = int(input('Digite um numero:'))
+print('Os numeros selecionados sao %d , %d , %d'%(num1, num2, num3))
+if (num1 > num2) and (num1 > num3) and (num2 > num3) :
+    print('A ordem sera %d, %d, %d'%(num1,num2,num3))
+elif (num1 > num3) and (num1 > num2) and (num3 > num2) :
+    print('A ordem sera %d, %d, %d'%(num1, num3, num2))
+elif (num2 > num3) and (num2 > num1) and (num3 > num1) :
+    print('A ordem sera %d, %d,%d'%(num2,num3,num1))
+elif (num2 > num1) and (num2 > num3) and (num1 > num3):
+    print('A ordem sera %d, %d, %d'%(num2, num1, num3))
+elif (num3 > num1) and (num3 > num2) and (num1 > num2) :
+    print('A ordem sera %d, %d, %d'%(num3, num1, num2))
+else :
+    print('A ordem sera %d, %d, %d'%(num3, num2, num1))
+   
