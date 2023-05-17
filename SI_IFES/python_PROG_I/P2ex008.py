8102b11a611dd9e17d7c61cdf727950baac00552
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-05-17 11:18:02 -0300

    g

--- SI_IFES/python_PROG_I/P2ex008.py
+++ SI_IFES/python_PROG_I/P2ex008.py
@@ -9,19 +9,19 @@ nota_menor = nota1
 if nota2 >= nota_maior :
     nota_maior = nota2
 elif nota2 <= nota_menor :
-    nota_maior = nota2
+    nota_menor = nota2
 if nota3 >= nota_maior :
     nota_maior = nota3
 elif nota3 <= nota_menor :
-    nota_maior = nota3
+    nota_menor = nota3
 if nota4 >= nota_maior :
     nota_maior = nota4
 elif nota4 <= nota_menor :
-    nota_maior = nota4
+    nota_menor = nota4
 if nota5 >= nota_maior :
     nota_maior = nota5
 elif nota5 <= nota_menor :
-    nota_maior = nota5
+    nota_menor = nota5
 media = ((nota1 + nota2 + nota3 + nota4 + nota5) - (nota_maior - nota_menor)) / 3
 print('A maior nota desse atleta foi %.2f, a menor foi %.2f e a media e %.2f'%(nota_maior,nota_menor,media))
 
