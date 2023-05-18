6d14e1952b71d48f1e470c3cb7ebe0a36acf3889
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-05-18 09:08:29 -0300

    gfd

---
+++ SI_IFES/js_scrpit/funcoes.js
@@ -0,0 +1,6 @@
+/* e um bloco de codigo projetado para uma tarefa especifica
+como se fosse mini-programinhas para contrinuir para o codigo*/
+
+function soma(valor1,valor2){
+    return valor1 + valor2;
+}
