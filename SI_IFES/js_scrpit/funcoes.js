2dae6647c17267c4139af60b8e82e72e775cd654
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-05-18 09:38:43 -0300

    aaaaaaa

--- SI_IFES/js_scrpit/funcoes.js
+++ SI_IFES/js_scrpit/funcoes.js
@@ -1,8 +1,27 @@
 /* e um bloco de codigo projetado para uma tarefa especifica
 como se fosse mini-programinhas para contrinuir para o codigo*/
-
+//Funçao de soma:
 function soma(valor1,valor2){
     return valor1 + valor2;
 }
 
 document.getElementById("texto").innerHTML = soma(10, 10);
+// function soma(valor1,valor2){
+//     return valor1 + valor2;
+// }
+
+// var total = soma(10, 23);
+
+// alert(total);
+
+//Funçao de Cotaçao do Dolar:
+// function realParaDolar (real,cotaçaoDolar){
+//     return real * cotaçaoDolar;
+// }
+
+// var valorReal = 7.89;
+// var cotacao = 5.09;
+
+// var total = realParaDolar(valorReal,cotacao);
+
+// alert("O valor em real e R$: "+ valorReal+"o valor em dolar e: U$ "+total);
