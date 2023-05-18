cbc13b210471faa99feb2f02625b4ef1793c29e8
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-05-18 17:58:25 -0300

    df

---
+++ SI_IFES/js_scrpit/funcoes.js
@@ -0,0 +1,7 @@
+function soma(valor1, valor2) {
+    return valor1 + valor2;
+}
+var valor1, valor2;
+valor1 = 4
+valor2 = 9
+document.getElementById("texto").innerHTML = "O valor dessa soma e " + soma(valor1, valor2);
