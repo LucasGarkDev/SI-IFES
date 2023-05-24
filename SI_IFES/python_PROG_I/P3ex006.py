0e587b1e95a56ae0b23552a76076f92420bf597d
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-05-24 11:24:45 -0300

    fgfg

--- SI_IFES/python_PROG_I/P3ex006.py
+++ SI_IFES/python_PROG_I/P3ex006.py
@@ -11,18 +11,28 @@ while num2 < 0 :
     num2 = int(input("Digite novamente: "))
 
 #calcular o MDC
-divisor = 2
-cont = 0
-quantidadeDeNumeros = 1
-while num1 != 0 and num2 != 0 :
-    if (divisor > 1) and (divisor // (2 ** (1/2)) == False) :
-        if (num1 % divisor == 0) and (num2 % divisor == 0):
-            acumulo = acumulo * divisor
-            cont = cont + 1
-            quantidadeDeNumeros = quantidadeDeNumeros + 1
-        else :
-            cont = cont + 1
-    else :
-        cont = cont + 1
-mdc = acumulo ** (1/quantidadeDeNumeros)
-print("O MDC desses dois numeros e: %.2f"%mdc)
+anterior = num1
+atual = num2
+resto = anterior % atual
+
+while resto != 0:
+    anterior = atual
+    atual = resto
+    resto = anterior % atual
+
+print("MDC(%d,%d)=%d" %(num1,num2,atual))
+# divisor = 2
+# cont = 0
+# quantidadeDeNumeros = 1
+# while num1 != 0 and num2 != 0 :
+#     if (divisor > 1) and (divisor // (2 ** (1/2)) == False) :
+#         if (num1 % divisor == 0) and (num2 % divisor == 0):
+#             acumulo = acumulo * divisor
+#             cont = cont + 1
+#             quantidadeDeNumeros = quantidadeDeNumeros + 1
+#         else :
+#             cont = cont + 1
+#     else :
+#         cont = cont + 1
+# mdc = acumulo ** (1/quantidadeDeNumeros)
+# print("O MDC desses dois numeros e: %.2f"%mdc)
