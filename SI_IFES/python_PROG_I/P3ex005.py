8133f76ac18ad5a5685e87865b14b0456b9dcb25
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-05-24 13:41:04 -0300

    Update P3ex005.py

--- SI_IFES/python_PROG_I/P3ex005.py
+++ SI_IFES/python_PROG_I/P3ex005.py
@@ -1,8 +1,10 @@
 #Pais A
 habitantesA = 80000
+taxaA = 0.03
 
 #Pais B
 habitantesB = 200000
+taxaB = 0.015
 
 #anos necessarios
 anos = 0
@@ -10,8 +12,8 @@ anos = 0
 #calculo de anos necessarios ate eles estarem com a mesma populaçao
 print("Estamos calculando.....")
 while habitantesA < habitantesB :
-    habitantesA += habitantesA * 0.03
-    habitantesB += habitantesB * 0.015
+    habitantesA = habitantesA + habitantesA * taxaA
+    habitantesB = habitantesB + habitantesB * taxaB
     anos = anos + 1 
 print("A quantidade de anos que eles precisaram para ficar a com a mesma populaçao foi...")
 print("...: %d anos "%anos)
