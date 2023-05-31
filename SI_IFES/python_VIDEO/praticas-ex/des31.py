e8d9b54be8e63f500571958841f2687fe42c7d42
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-05-31 11:20:54 -0300

    fg

--- SI_IFES/python_VIDEO/praticas-ex/des31.py
+++ SI_IFES/python_VIDEO/praticas-ex/des31.py
@@ -12,18 +12,17 @@ while num <= 0 :
     num = int(input("Digite um numero: "))
 
 #verificar se ele e primo
-if num > 1 :
-    while divisor < num : #para o numero ser um numero primo o num e o divisor devem ser iguais
-        if num % divisor == 0 :#nao podendo ser divisiveis, um numero primo so divide por ele mesmo
+if num > 1 :#toda a funçao abaixo apenas diz que o numero nao e primo, se nao cair na condiçao, entao e primo
+    while divisor < num : #para o numero ser primo ele so pode dividir por ele,nunca nada abaixo dele
+        if num % divisor == 0 :#caso ele caia aqui nos ja sabemos que ele nao e primo
             numPrimo = False
             print("O numero %d nao e Primo"%num)
-            divisor = num + 1
+            divisor = num + 1 #isso vai servir para quebrar esse loop
         else :
             divisor = divisor + 1
-
+    
     if numPrimo == True :
-        print("O numero %d e Primo"%num)
-
+        print("O numero %d e primo"%num)       
 else :
-    print("Para um numero ser primo ele precisa ser maior que 1") 
-    num = int(input("Digite novamente: "))
+    print("Para esse numero ser primo...")
+    print("Ele precisa ser maior que 1")
