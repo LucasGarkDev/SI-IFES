17e1a29605cbeaed143a76acace8bca91b29d047
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-05-30 21:57:19 -0300

    ghfgh

--- SI_IFES/python_VIDEO/praticas-ex/des19.py
+++ SI_IFES/python_VIDEO/praticas-ex/des19.py
@@ -5,3 +5,26 @@
 # ao dobro do percentual do ano anterior. Faça um programa que determine o salário 
 # atual desse funcionário. Após concluir isto, altere o programa permitindo que o 
 # usuário digite o salário inicial do funcionário.
+
+#inicializaçao
+ano = 1995
+salario = float(input("Digite o valor do salario que o funcionario tinha em 1995: "))
+while salario <= 0 :
+    print("Salario nao compativel")
+    salario = float(input("Digite novamente: "))
+taxaSalarial = 0.015
+
+
+#calculo do salario dos anos posteriores
+anoAtual = int(input("Digite qual ano estamos atualmente: "))
+while anoAtual <= 1995 :
+    print("Esse funcionario so trabalhou nessa empresa apartir de 1995")
+    anoAtual = int(input("Digite qual ano estamos atualmente: "))
+while ano < anoAtual :
+    taxaSalarial = taxaSalarial * 2
+    aumento = salario * taxaSalarial
+    salario = salario + aumento
+    ano = ano + 1
+print("O salario que ficara em %2.2f apos chegar no ano: %d "%(salario,ano,))
+print("Sendo aplicado uma taxa total de %3.3f"%taxaSalarial)
+    
--- SI_IFES/python_VIDEO/praticas-ex/des20.py
+++ SI_IFES/python_VIDEO/praticas-ex/des20.py
@@ -11,3 +11,69 @@
 # A percentagem de votos nulos sobre o total de votos;
 # A percentagem de votos em branco sobre o total de votos. 
 # Para finalizar o conjunto de votos tem-se o valor zero.
+
+#perguntar quantas pessoas vao votar
+quantVotos = int(input("Digite a quantidade de pessoas que vao votar, hoje: "))
+while quantVotos <= 0 :
+    print("Quantidade invalida")
+    quantVotos = int(input("Digite novamente: "))
+
+#pedir a tabela
+tabela = str(input("Voce deseja ver a tabela(sim/nao)? ")).upper()
+#tabela dos candidatos
+if tabela == "SIM" :
+    print("1 - Richiele")
+    print("2 - Eliane")
+    print("3 - Lastenio")
+    print("4 - Ze da Egua")
+    print("5 - Voto Nulo")
+    print("6 - Voto em Branco")
+voto1 = 0
+voto2 = 0
+voto3 = 0
+voto4 = 0
+voto5 = 0
+voto6 = 0
+
+#pedir o codigo
+contVotos = 1
+while contVotos <= quantVotos :
+    codigo = int(input("Digite o codigo para confirmar o seu voto: "))
+    while codigo != 1 and codigo != 2 and codigo != 3 and codigo != 4 and codigo != 5 and codigo != 6 :
+        print("Codigo invalido")
+        codigo = int(input("Digite digite novamente: "))
+
+    #quantidade de votos para cada candidato
+    if codigo == 1 :
+        voto1 = voto1 + 1
+    elif codigo == 2 :
+        voto2 = voto2 + 1
+    elif codigo == 3 :
+        voto3 = voto3 + 1
+    elif codigo == 4 :
+        voto4 = voto4 + 1
+    elif codigo == 5 :
+        voto5 = voto5 + 1
+    else :
+        voto6 = voto6 + 1
+
+    contVotos = contVotos + 1
+
+#porcentagem de votos nulos e em branco sobre o total de votos 
+calculoNulos = (voto5 * 100) / contVotos
+calculoBranco = (voto6 * 100) / contVotos
+
+#leitura final
+print("A quantidade de votos do candidato 1: %d"%voto1)
+print("A quantidade de votos do candidato 2: %d"%voto2)
+print("A quantidade de votos do candidato 3: %d"%voto3)
+print("A quantidade de votos do candidato 4: %d"%voto4)
+print("A quantidade de votos nulos e: %d"%voto5)
+print("A quantidade de votos em branco e: %d"%voto6)
+print("A porcentagem de votos nulos em relaçao ao total foi: %.2f"%calculoNulos)
+print("A porcentagem de votos em branco em relaçao ao total foi: %.2f"%calculoBranco)
+
+
+
+
+
