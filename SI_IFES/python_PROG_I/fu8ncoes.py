adb4181b356c33cd40416d2d8ceba88ece32ba0e
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-06-05 11:22:02 -0300

    fgfgh

--- SI_IFES/python_PROG_I/fu8ncoes.py
+++ SI_IFES/python_PROG_I/fu8ncoes.py
@@ -4,3 +4,24 @@ def organizar() :
 organizar()
 print("Inicializaçao")
 organizar()
+
+def criar_comentario(comentario):
+    """
+    Cria um comentário de uma linha.
+
+    Argumento:
+    - comentario (str): O conteúdo do comentário.
+
+    Retorna:
+    Uma string com o comentário formatado.
+
+    Exemplo de uso:
+    comentario = criar_comentario("Este é um comentário de exemplo.")
+    print(comentario)
+    # Saída: "# Este é um comentário de exemplo."
+    """
+    return "# " + comentario
+
+comentario = criar_comentario("Este é um comentário de exemplo.")
+print(comentario)
+# Saída: "# Este é um comentário de exemplo."
