d4b17e125ed64c530ae3f8cf0f6abfb1f5f4d85e
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-06-07 17:34:36 -0300

    Update funcoes.js

--- SI_IFES/js_scrpit/funcoes.js
+++ SI_IFES/js_scrpit/funcoes.js
@@ -1,7 +1,23 @@
-function soma(valor1, valor2) {
+/*function soma(valor1, valor2) {
     return valor1 + valor2;
 }
 var valor1, valor2;
 valor1 = 4
 valor2 = 9
-document.getElementById("texto").innerHTML = "O valor dessa soma e " + soma(valor1, valor2);
+document.getElementById("texto").innerHTML = "O valor dessa soma e " + soma(valor1, valor2);*/
+/*function realParaDolar(precoReal,covnDolar){
+    precoDolar = precoReal * covnDolar
+    return precoDolar
+}
+var precoReal, covnDolar;
+precoReal = 19
+covnDolar = 4.92
+    
+
+document.getElementById("texto").innerHTML = "O valor e:" + realParaDolar(precoReal,covnDolar)*/
+
+function minhaFuncao(val){
+    return (5/9)*(val-32);
+}
+var val = 12
+alert("O valor e: " + val + ".." );
