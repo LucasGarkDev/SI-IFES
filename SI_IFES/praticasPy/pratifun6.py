6db2353ef0ddd3356be12a71f2ea59e700a021a4
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-06-19 08:30:16 -0300

    4ff4

--- SI_IFES/praticasPy/pratifun6.py
+++ SI_IFES/praticasPy/pratifun6.py
@@ -4,3 +4,34 @@
 # positivo ao usuário, n. Então uma função exibe todos os termos da 
 # sequência até o n-ésimo termo. Use recursividade.
 
+#------------------------funçoes----------------------
+#pedir o tamanho da sequencia
+def pedirN():
+    n = int(input("Digite um numero: "))
+    while n <= 0 :
+        print("Esse numero nao e valido")
+        n = int(input("Digite novamente: "))
+    return n
+
+#execussao da sequencia
+def sequencia(n):
+    n1 = 0
+    n2 = 1
+    proxN = n1 + n2
+    print(n1,n2,proxN, end=" ")
+    if n > 3 :
+        cont = 3
+        while cont < n :
+            n1 = n2
+            n2 = proxN
+            proxN = n1 + n2
+            print(proxN, end=" ")
+            cont = cont + 1
+        
+
+
+#------------------------main-------------------------
+def main():
+    num = pedirN()
+    sequencia(num)
+main()
