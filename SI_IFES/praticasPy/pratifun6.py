2f5bbb8e8b7e54ed39779df63bb04ff707cda540
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-06-19 08:14:58 -0300

    5gg5

---
+++ SI_IFES/praticasPy/pratifun6.py
@@ -0,0 +1,6 @@
+# A série de Fibonacci é uma sequência de números, cujos dois primeiros 
+# são 0 e 1. O termo seguinte da sequência é obtido somando os dois 
+# anteriores. Faça uma script em Python que solicite um inteiro 
+# positivo ao usuário, n. Então uma função exibe todos os termos da 
+# sequência até o n-ésimo termo. Use recursividade.
+
