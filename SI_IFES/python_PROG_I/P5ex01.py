a327537458a39f8c059c1eedfd1b6c17529d2168
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-06-26 11:09:03 -0300

    rffg

--- SI_IFES/python_PROG_I/P5ex01.py
+++ SI_IFES/python_PROG_I/P5ex01.py
@@ -12,30 +12,34 @@ def gerarNumAleatorio(min,max):
 
 
 #repetir a escolha de numeros e guardar no vetor
-def guardarVetor(numeros,par,impar):
-    min = 1
-    max = 100
+def guardarVetor(numeros,min,max):
     cont = 0
+    par = 0
+    impar = 0
     while cont < 50 :
         n = gerarNumAleatorio(min,max)
         numeros.append(n)
-        parOuImpar(n,par,impar)
+        par = percorreNum(numeros,n,int(par),int(impar))
+        impar = percorreNum(numeros,n,int(par),int(impar))
         cont = cont + 1
-
+    r1 = contPar(int(par))
+    r2 = contImpar(int(impar))
+    imprimir(r1,r2)
 
 #percorrer os numeros
-def percorreNum(num):
-    i = 0 
-    while i < 50: 
-       i = i + 1
+def percorreNum(numeros,n,par,impar):
+    for n in numeros :
+        parOuImpar(n,par,impar)
 
         
 #verificar se e par ou impar
 def parOuImpar(n,par,impar):
     if n % 2 == 0 :
-        par.append(n)
+        par = par + 1
+        return par
     else :
-        impar.append(n)
+        impar = impar + 1
+        return impar
 
 #contar quantos pares 
 def contPar(p): 
@@ -55,12 +59,7 @@ def imprimir(r1,r2):
 #-----------------------main---------------------------
 def main():
     numeros = []
-    par = []
-    impar = []
-    guardarVetor(numeros,par,impar)
-    percorreNum(numeros)
-    r1 = contPar(par)
-    r2 = contImpar(impar)
-    imprimir(r1,r2)
-    
+    min = 1
+    max = 100
+    guardarVetor(numeros,min,max)
 main()
