bf94d7586bd582931d6780a1872c1673454bcb29
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-06-26 18:01:40 -0300

    Create P5ex07.py

---
+++ SI_IFES/python_PROG_I/P5ex07.py
@@ -0,0 +1,30 @@
+# Uma grande emissora de televisão quer fazer uma enquete entre os seus telespectadores
+# para saber qual o melhor jogador após cada jogo. Para isto, faz-se necessário o
+# desenvolvimento de um programa, que será utilizado pelas telefonistas, para a
+# computação dos votos. Sua equipe foi contratada para desenvolver este programa,
+# utilizando a linguagem de programação Python. Para computar cada voto, a telefonista
+# digitará um número, entre 1 e 25, correspondente ao número da camisa do jogador. Um
+# número de jogador igual zero, indica que a votação foi encerrada. Se um número inválido
+# for digitado, o programa deve ignorá-lo, mostrando uma breve mensagem de aviso, e
+# voltando a pedir outro número. Após o final da votação, o programa deverá exibir:
+#  O total de votos computados;
+#  Os números e respectivos votos de todos os jogadores que receberam votos;
+#  O percentual de votos de cada um destes jogadores;
+#  O número do jogador escolhido como o melhor jogador da partida, juntamente com
+# o número de votos e o percentual de votos dados a ele.
+# DICA 1: Utilize um vetor para armazenar os votos. O índice do vetor é o número do
+# jogador. Então quando um jogador recebe um voto, o elemento do vetor
+# correspondente a essa posição soma mais um.
+# DICA 2: Esse vetor deve ser inicializado com ZERO nas 25 posições.
+# OBS: Observe que os votos inválidos e o zero final não devem ser computados como
+# votos. O resultado aparece ordenado pelo número do jogador. O programa deve fazer
+# uso de vetores. O programa deverá executar o cálculo do percentual de cada jogador
+# através de uma função. Esta função receberá dois parâmetros: o número de votos de
+# um jogador e o total de votos. A função calculará o percentual e retornará o valor
+# calculado. Abaixo segue uma tela de exemplo.
+
+#-----------------------funçoes------------------------
+
+
+#-----------------------main---------------------------
+
