21dc242c8d06b463a587534f32f98c617991cf38
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-06-28 08:39:54 -0300

    rrffr

--- SI_IFES/python_PROG_I/P5ex07.py
+++ SI_IFES/python_PROG_I/P5ex07.py
@@ -51,18 +51,19 @@ def imprimirTotal(v):
 
 #imprimir os numeros e os votos dos jogadores que foram votados
 def imprimriNumVotos(votos):
-    print("Jogador:")
-    imprimirNum(votos)
+    print("Jogador:", end="     ")
     print("Pontos:")
+    imprimirNum(votos)
     imprimirPontos(votos)
 
 #imprimir os numeros
 def imprimirNum(v):
     i = 0
+    jog = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25]
     del(v[0])
     while i < len(v):
         if v[i] != 0 :
-            print(v[i])
+            print(jog[i], end="     ")
             print(" "*30)
             i = i + 1
         else:
@@ -74,7 +75,7 @@ def imprimirPontos(v):
     del(v[0])
     while i < len(v):
         if v[i] != 0 :
-            print(i)
+            print(v[i])
             print(" "*30)
             i = i + 1
         else:
