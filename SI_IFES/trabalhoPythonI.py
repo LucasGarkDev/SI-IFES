002c9fee8c35f48e037a6bf615bbff302fd80e76
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-07-31 07:15:06 -0300

    jk

--- SI_IFES/trabalhoPythonI.py
+++ SI_IFES/trabalhoPythonI.py
@@ -35,20 +35,27 @@ def inserirProduto(vetor1,vetor2):
     vetor2.append(preco)
 
 def pedirCodigo(vetor1):
-    codigo = int(input("Digite o numero do codigo: "))
-    ver = pesquisar(vetor1,codigo) 
+    codigo = input("Digite o numero do codigo: ")
+    while codigo.isdigit() == False:
+        print("Não é possível usar letras, palavras ou números com vírgula aqui.")
+        codigo = input("Digite o numero do codigo, novamente: ")
+    
+    codigo = int(codigo)
+    ver = pesquisar(vetor1, codigo) 
     while ver != -1 :
         print("Codigo ja existente")
         codigo = int(input("Digite o numero do codigo, novamente: "))
-        ver = pesquisar(vetor1,codigo)
+        ver = pesquisar(vetor1, codigo)
     return codigo
-
+    
+        
 def pedirPreco():
     preco = float(input("Digite o preço desse produto: "))
-    while preco <= 0 :
-        print("Preço impossivel de existi no estoque")
+    while preco <= 0 or preco.decimal() == False:
+        print("Preço impossível de existir no estoque")
         preco = float(input("Digite o preço desse produto, novamente: "))
     return preco
+
 
 def imprimirResPesquisa(item, vetor1, vetor2, i):
     if item == -1:
@@ -58,6 +65,7 @@ def imprimirResPesquisa(item, vetor1, vetor2, i):
         print("O produto de código %d tem o preço: %.2f" % (vetor1[item], vetor2[item]))
         print("----------------------------------")
 
+
 def somar(vetor2):
     i = 1
     soma = 0
@@ -66,27 +74,44 @@ def somar(vetor2):
         i += 1
     return soma
 
+
 def porcentagem(indice, vetor2, porcentagem):
     cal = vetor2[indice]
     aumento = cal * (porcentagem / 100)
     return cal + aumento
 
+
+def obter_codigo_valido(vetor1):
+    codigo = input("Digite o código do produto: ")
+
+    if codigo.isdigit():
+        codigo_int = int(codigo)
+        for cod in vetor1:
+            if cod == codigo_int:
+                return codigo_int
+
+    print("Código inválido ou não encontrado no vetor.")
+    return obter_codigo_valido(vetor1)
+
+
 def aumentarPreco(vetor2, vetor1):
-    codigo = int(input("Digite o código do produto: "))
+    codigo = obter_codigo_valido(vetor1)
+
+    percentual = input("Digite o valor da porcentagem de aumento: ")
+    while codigo.isdigit() == False or float(percentual) <= 0:
+        print("Porcentagem inválida.")
+        percentual = input("Digite o valor da porcentagem de aumento novamente: ")
+
+    codigo = int(codigo)
     indice = pesquisar(vetor1, codigo)
-    percentual = float(input("Digite o valor da porcentagem de aumento: "))
-    while percentual <= 0:
-        print("Porcentagem inválida")
-        percentual = float(input("Digite o valor da porcentagem de aumento: "))
-    calporcentagem = porcentagem(indice, vetor2, percentual)
-    if indice == -1:
-        print("O produto de código %d não foi encontrado" % codigo)
-    else:
-        print("-----------------------------------------")
-        print("     O valor do produto %d subirá de %.2f \n" % (vetor1[indice], vetor2[indice]))
-        vetor2[indice] = calporcentagem
-        print("     para %.2f." % vetor2[indice])
-        print("-----------------------------------------")
+    calporcentagem = porcentagem(indice, vetor2, float(percentual))
+
+    print("-----------------------------------------")
+    print("     O valor do produto %d subirá de %.2f \n" % (vetor1[indice], vetor2[indice]))
+    vetor2[indice] = calporcentagem
+    print("     para %.2f." % vetor2[indice])
+    print("-----------------------------------------")
+    
 
 def maiorPreco(vetor2,vetor1):
     maiorPreco = 0  
@@ -105,8 +130,14 @@ def maiorPreco(vetor2,vetor1):
     else:
         print("Nenhum produto encontrado")
 
-def excliur(vetor1,vetor2):
-    codigo = int(input("Digite o código do produto a ser excluído: "))
+
+def excliur(vetor1, vetor2):
+    codigo = input("Digite o código do produto a ser excluído: ")
+    while codigo.isdigit() == False:
+        print("Não é possível usar letras, palavras ou números com vírgula aqui.")
+        codigo = input("Digite o código do produto a ser excluído, novamente: ")
+    
+    codigo = int(codigo)
     indice = pesquisar(vetor1, codigo)
     if indice != -1:
         del(vetor1[indice])
@@ -114,12 +145,24 @@ def excliur(vetor1,vetor2):
         print("Produto de código %d excluído com sucesso!" % codigo)
     else:
         print("Produto de código %d não encontrado." % codigo)
+    
+
+def listar(vetor1, vetor2):
+    i = 0
+    o = 1
+    print("----------------------PRODUTOS----------------------")
+    while i < len(vetor1):
+        print("O vetor de ordem %d tem o codigo %d" % (o, vetor1[i]))
+        print("E possui o preço em %.2f" % vetor2[i])
+        print("-----------------------------------------------------")
+        i += 1
+        o += 1
 
 ###### PRINCIPAL ##########
 def main():
     op = 1
-    vetorCod = [0]
-    vetorPreco = [0]
+    vetorCod = []
+    vetorPreco = []
     while op != 0:
         op = menu()
         
@@ -140,31 +183,40 @@ def main():
 
         elif op == 3:
             print("\n\nAUMENTAR O PREÇO\n\n")
-            aumentarPreco(vetorPreco,vetorCod)
+            if len(vetorCod) == 0:
+                print("Não a nada no vetor para poder aumentar")
+            else:
+                aumentarPreco(vetorPreco,vetorCod)
             
 
 
         elif op == 4:
             print("\n\nMAIOR\n\n")
-            maiorPreco(vetorPreco,vetorCod)
+            if len(vetorCod) == 0:
+                print("Não a nada no vetor para exisitir um maior")
+            else:
+                maiorPreco(vetorPreco,vetorCod)
 
 
 
         elif op == 5:
             print("\n\nEXCLUIR\n\n")
-            excliur(vetorCod,vetorPreco)
+            if len(vetorCod) == 0:
+                print("Não a nada no vetor para poder excluir")
+            else:
+                excliur(vetorCod,vetorPreco)
 
 
         elif op == 6:
             print("\n\nLISTAR\n\n")
-            # Listar todos os dados dos vetores
+            if len(vetorCod) == 0:
+                print("Não a nada no vetor para poder listar")
+            else:
+                listar(vetorCod,vetorPreco)
 
 
         else:
             print("Opção inválida!")
 
         input("Pressione <enter> para continuar ...")
-    
-
-
 main()
