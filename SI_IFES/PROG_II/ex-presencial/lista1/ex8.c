82b906be012e4ae9ab9ddfc26bc68b524fe25be0
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-08-17 17:48:38 -0300

    Update ex8.c

--- SI_IFES/PROG_II/ex-presencial/lista1/ex8.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/ex8.c
@@ -1,13 +1,3 @@
-/*Uma academia deseja fazer um senso entre seus clientes para descobrir o 
-mais alto, o mais baixo, o mais gordo e o mais magro. Para isto 
-você deve fazer um programa que pergunte a cada um dos clientes 
-da academia seu código, sua altura e seu peso. O final da digitação de
-dados deve ser dado quando o usuário digitar 0 (zero) no campo código. 
-Ao encerrar o programa devem ser informados os códigos e valores 
-do cliente mais alto, do mais baixo, do mais gordo e do mais magro, 
-além da média das alturas e dos pesos dos clientes.
-*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -16,90 +6,47 @@ além da média das alturas e dos pesos dos clientes.
 #define inicio "--------INICIO--------"
 #define resultado "-----------RESULTADO-----------"
 #define corte "-------------------------------------"
+#define tam 5
 
-int pedirCodigo(int cont);
-float pedirAltura(int cont);
-float pedirPeso(int cont);
-void inserirClientes(int codigos[5],float altura[5],float pesos[5]);
-float descobreMagroBaixo(int codigos[5],float vetor[5]);
-float descobreAltoGordo(int codigos[5],float vetor[5]);
+// int pedirQtde();
+int pedirMatricula(int cont);
+float pedirNota(int cont);
+char pedirSexo(int cont);
+int classificacao();
+char contagemH(char vetor[tam]);
 
 int main() {
     printf("\n%s\n", inicio);
-    int vetorCodigo[5];
-    float vetorAltura[5], vetorPeso[5];
-    inserirClientes(vetorCodigo,vetorAltura,vetorPeso);
-    float alto, magro, gordo, baixo;
-    alto = descobreAltoGordo(vetorCodigo,vetorAltura);
-    gordo = descobreAltoGordo(vetorCodigo,vetorPeso);
-    baixo = descobreMagroBaixo(vetorCodigo,vetorAltura);
-    magro = descobreMagroBaixo(vetorCodigo,vetorPeso);
+    int quanti, matricula, i;
+    float nota;
+    char sexo;
+    // quanti = pedirQtde();
+    int matriculas[tam];
+    float notas[tam];
+    char sexos[tam];
+    for (i = 1; i <= tam; i++){
+        matricula = pedirMatricula(i);
+        matriculas[i] = matricula;
+        nota = pedirNota(i);
+        notas[i] = nota
+        sexo = pedirSexo(i);
+        sexos[i] = sexo;
+    }
     printf("\n%s\n", resultado);
-    printf("O mais alto tem: %.2f\n", alto);
-    printf("O mais baixo tem: %.2f\n", baixo);
-    printf("O mais magro tem: %.2f\n", magro);
-    printf("O mais gordo tem: %.2f\n", gordo);
+    
     printf("\n%s\n", corte);
     return 0;
 }
 
-void inserirClientes(int codigos[5],float altura[5], float pesos[5]){
-    int i;
-    for (i = 0; i < 5; i++){
-        codigos[i] = pedirCodigo(i);
-        altura[i] = pedirAltura(i);
-        pesos[i] = pedirPeso(i);
-    } 
-}
-
-
-float descobreMagroBaixo(int codigos[5],float vetor[5]){
-    int i;
-    float variavel;
-    variavel = 0;
-    for (i = 0; i < 5; i++){
-        if (vetor[i] < variavel){
-            variavel =  vetor[i];
-        }
-    }
-    return variavel;
-}
-
-float descobreAltoGordo(int codigos[5],float vetor[5]){
-    int i;
-    float variavel;
-    variavel = 0;
-    for (i = 0; i < 5; i++){
-        if (vetor[i] > variavel){
-            variavel =  vetor[i];
-        }
-    }
-    return variavel;
-}
-
-int pedirCodigo(int cont){
+int pedirMatricula(int cont){
     int num;
     do{
-        printf("Digite o codigo do aluno %d: ", cont);
+        printf("Digite o numero de matricula do aluno %d: ", cont);
         scanf("%d", &num);
     } while (num <= 0);
     return num;
 }
 
-float pedirAltura(int cont){
-    float num;
-    do{
-        printf("Digite a altura do aluno %d: ", cont);
-        scanf("%f", &num);
-    } while (num <= 0);
-    return num;
-}
-
-float pedirPeso(int cont){
-    float num;
-    do{
-        printf("Digite o peso do aluno %d: ", cont);
-        scanf("%f", &num);
-    } while (num <= 0);
-    return num;
+char contagemH(char vetor[tam]){
+    
 }
