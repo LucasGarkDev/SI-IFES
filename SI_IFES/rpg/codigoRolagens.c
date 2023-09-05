92a4f77fe806e22ea44ee701be62ad43825cc12e
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-09-05 09:42:22 -0300

    gfgf

--- SI_IFES/rpg/codigoRolagens.c
+++ SI_IFES/rpg/codigoRolagens.c
@@ -3,106 +3,109 @@
 #include <time.h>
 #include <string.h>
 
-// Estrutura para representar um tipo de ataque
 struct TipoAtaque {
     char nome[50];
     int num_ataques;
-    int dado_dano; // Armazena o número de lados do dado de dano
-    int mod_habilidade; // Modificador de habilidade para este tipo de ataque
+    int num_rolagens;
+    int lados_dado;
+    int mod_habilidade;
 };
 
-// Estrutura para representar um monstro
 struct Monstro {
     char nome[50];
     int quantidade;
     int num_tipos_ataque;
-    struct TipoAtaque tipos_ataque[5]; // Supomos até 5 tipos de ataques por monstro
+    struct TipoAtaque tipos_ataque[5];
 };
 
-// Função para rolar um dado com um número específico de lados
-int rolarDado(int lados) {
-    return rand() % lados + 1;
+int rolarD20() {
+    return rand() % 20 + 1;
 }
 
-// Função para simular os ataques de um monstro
-void simularAtaques(struct Monstro monstro, int monstro_num) {
-    printf("Simulando ataques do monstro %s (Monstro #%d):\n", monstro.nome, monstro_num + 1);
-    
-    for (int i = 0; i < monstro.quantidade; i++) {
-        int total_dano = 0;
-        for (int j = 0; j < monstro.num_tipos_ataque; j++) {
-            printf("Tipo de Ataque #%d: %s\n", j + 1, monstro.tipos_ataque[j].nome);
-            for (int k = 0; k < monstro.tipos_ataque[j].num_ataques; k++) {
-                int rolagem = rolarDado(20); // Rola um dado de 20 lados
-                int dano = rolarDado(monstro.tipos_ataque[j].dado_dano); // Rola o dado de dano
-                // Adiciona o modificador de habilidade específico para este tipo de ataque
-                dano += monstro.tipos_ataque[j].mod_habilidade;
-                total_dano += dano;
-                printf("Ataque %d: Rolagem = %d, Dano = %d\n", k + 1, rolagem, dano);
-            }
-        }
-        printf("Dano total causado por este monstro: %d\n", total_dano);
+int rolarDado(int num_rolagens, int lados_dado) {
+    int resultado = 0;
+    for (int i = 0; i < num_rolagens; i++) {
+        resultado += rand() % lados_dado + 1;
     }
-    printf("\n");
+    return resultado;
 }
 
 int main() {
-    srand(time(NULL)); // Inicializa o gerador de números aleatórios
-    
-    int opcao;
+    srand(time(NULL));
+    printf("Bem-vindo ao RPG Monster Roller!\n");
+
+    int num_monstros;
+    printf("Quantos monstros deseja adicionar? ");
+    scanf("%d", &num_monstros);
+
     struct Monstro monstros[10];
-    int num_monstros = 0;
-    
+
+    for (int num = 0; num < num_monstros; num++) {
+        struct Monstro monstro;
+        printf("\nMonstro #%d\n", num + 1);
+        printf("Nome do monstro: ");
+        scanf("%s", monstro.nome);
+        printf("Quantidade de %s: ", monstro.nome);
+        scanf("%d", &monstro.quantidade);
+
+        printf("Quantos tipos de ataque diferentes esse monstro tem? ");
+        scanf("%d", &monstro.num_tipos_ataque);
+
+        for (int i = 0; i < monstro.num_tipos_ataque; i++) {
+            printf("Digite o nome do Tipo de Ataque #%d: ", i + 1);
+            scanf("%s", monstro.tipos_ataque[i].nome);
+            printf("Digite o numero de ataques para este tipo: ");
+            scanf("%d", &monstro.tipos_ataque[i].num_ataques);
+            printf("Digite o dado de dano para este tipo (no formato XdY): ");
+            scanf("%dd%d", &monstro.tipos_ataque[i].num_rolagens, &monstro.tipos_ataque[i].lados_dado);
+            printf("Digite o modificador de habilidade para este tipo: ");
+            scanf("%d", &monstro.tipos_ataque[i].mod_habilidade);
+        }
+
+        monstros[num] = monstro;
+    }
+
+    int opcao;
     do {
-        printf("Menu:\n");
-        printf("1 - Adicionar monstro\n");
-        printf("2 - Iniciar rodada\n");
-        printf("3 - Sair\n");
+        printf("\nMenu:\n");
+        printf("1 - Iniciar rodada\n");
+        printf("2 - Sair\n");
         printf("Escolha uma opcao: ");
         scanf("%d", &opcao);
-        
+
         switch (opcao) {
             case 1:
-                if (num_monstros < 10) {
-                    printf("Digite o nome do monstro: ");
-                    scanf("%s", monstros[num_monstros].nome);
-                    printf("Digite a quantidade de monstros: ");
-                    scanf("%d", &monstros[num_monstros].quantidade);
-                    printf("Digite o numero de tipos de ataques do monstro: ");
-                    scanf("%d", &monstros[num_monstros].num_tipos_ataque);
-                    
-                    for (int i = 0; i < monstros[num_monstros].num_tipos_ataque; i++) {
-                        printf("Digite o nome do Tipo de Ataque #%d: ", i + 1);
-                        scanf("%s", monstros[num_monstros].tipos_ataque[i].nome);
-                        printf("Digite o numero de ataques para este tipo: ");
-                        scanf("%d", &monstros[num_monstros].tipos_ataque[i].num_ataques);
-                        printf("Digite o numero de lados do dado de dano para este tipo: ");
-                        scanf("%d", &monstros[num_monstros].tipos_ataque[i].dado_dano);
-                        printf("Digite o modificador de habilidade para este tipo: ");
-                        scanf("%d", &monstros[num_monstros].tipos_ataque[i].mod_habilidade);
+                for (int num = 0; num < num_monstros; num++) {
+                    struct Monstro monstro = monstros[num];
+                    printf("\n%s:\n", monstro.nome);
+                    for (int i = 0; i < monstro.num_tipos_ataque; i++) {
+                        struct TipoAtaque tipo_ataque = monstro.tipos_ataque[i];
+                        printf("Tipo de Ataque: %s\n", tipo_ataque.nome);
+                        printf("Quantidade de ataques: %d\n", tipo_ataque.num_ataques);
+                        printf("Resultado das rolagens:\n");
+
+                        for (int j = 0; j < monstro.quantidade; j++) {
+                            int ataque = rolarD20() + tipo_ataque.mod_habilidade;
+                            printf("Ataque #%d - Monstro #%d (D20 para acerto): %d\n", i + 1, j + 1, ataque);
+
+                            for (int k = 0; k < tipo_ataque.num_ataques; k++) {
+                                int resultado_dano = rolarDado(tipo_ataque.num_rolagens, tipo_ataque.lados_dado) + tipo_ataque.mod_habilidade;
+                                printf("Ataque #%d - Monstro #%d - Rolagem de Dano #%d: %d\n", i + 1, j + 1, k + 1, resultado_dano);
+                            }
+                        }
                     }
-                    
-                    num_monstros++;
-                } else {
-                    printf("Limite de monstros atingido (max 10).\n");
                 }
                 break;
+
             case 2:
-                if (num_monstros > 0) {
-                    for (int i = 0; i < num_monstros; i++) {
-                        simularAtaques(monstros[i], i);
-                    }
-                } else {
-                    printf("Nenhum monstro registrado.\n");
-                }
+                printf("Saindo do RPG Monster Roller. Ate a proxima!\n");
                 break;
-            case 3:
-                printf("Saindo...\n");
-                break;
+
             default:
-                printf("Opcao invalida.\n");
+                printf("Opcao invalida. Tente novamente.\n");
         }
-    } while (opcao != 3);
-    
+    } while (opcao != 2);
+
     return 0;
 }
+
