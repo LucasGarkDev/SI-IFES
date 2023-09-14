578e42ad41807567c51303d88d7e63165b1895f4
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-09-14 18:08:19 -0300

    Update ex4.c

--- SI_IFES/PROG_II/ex-presencial/lista1/lista6/ex4.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista6/ex4.c
@@ -1,31 +1,61 @@
 /*Implemente a função contCaractere, que dado uma string e um caractere, 
 determina quantas vezes o caractere aparece na string. */
 
-#include <stdio.h>
-#include <string.h>
-#define TAM 50
+/*Implemente  manualmente  a função  strlen, ou seja, 
+uma função que conta a quantidade de caracteres de uma 
+string (obviamente não é permitido o uso da função strlen 
+da biblioteca <string.h>*/
 
-char pedirCaracter(){
-    char letra;
-    printf("Digite o caracter que deseja pesquisar: ");
-    scanf("%c", &letra);
-    return letra;
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+#define INICIO "--------INICIO--------"
+#define RESULTADO "-----------RESULTADO-----------"
+#define CORTE "-------------------------------------"
+// #define TAM 21
+
+int pedirQtde(){
+    int num;
+    do{
+        printf("Digite a quantidade de espaços no vetor: ");
+        scanf("%d", &num);
+    } while (num <= 0);
+    return num;
+}
+
+void limparBuffer(){
+    int ch;
+    do{
+        ch = fgetc(stdin);
+    } while (ch != EOF && ch != '\n');
 }
 
 int main() {
-    char str[50] = "Exemplo de string para teste";
-    char * pch;	// Ponteiro para char
-    int cont = 0;
-    char pesq = pedirCaracter;
-    printf ("Procurando o caractere '%c' na string %s\n",pesq, str);
-    pch = strchr(str,pesq);
-    while ( pch != NULL)
-    {
-        printf ("encontrado na posição %d\n", pch - str);  
-        pch = strchr(pch+1, pesq);
-        cont++;
-              
+    char *str;
+    char achado;
+    int count = 0, i;
+    
+    int quanti = pedirQtde();
+    str = (char *) malloc(quanti * sizeof(char)); 
+    limparBuffer();
+    
+    printf("Informe os caracteres: ");
+    fgets(str, quanti, stdin);
+    limparBuffer();
+    
+    printf("%s\n", str);
+    printf("Informe o caractere que deseja buscar: ");
+    scanf("%c", &achado);
+    
+    for(i = 0; str[i] != '\0'; i++) {
+        if (achado == str[i]){
+            count++;
+        }
     }
-    printf("O numero de letras %c sao: %d",pesq, cont);
 
+    printf("Quantidade de %c = %d", achado, count);
+    free(str);
+    
+    return 0;
 }
