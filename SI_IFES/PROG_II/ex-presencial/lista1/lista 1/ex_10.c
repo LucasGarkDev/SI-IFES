23429720d77e634f832325106072216899af824d
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-09-16 17:37:11 -0300

    jhkhjk

--- SI_IFES/PROG_II/ex-presencial/lista1/lista 1/ex_10.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista 1/ex_10.c
@@ -6,53 +6,48 @@ um fumante perderá. Exiba o total de dias.*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <windows.h>
+#include <math.h>
+#include <ctype.h>
 
-#define inicio "--------INICIO--------"
-#define resultado "-----------RESULTADO-----------"
-#define corte "-------------------------------------"
+#define INICIO "--------INICIO--------"
+#define RESULTADO "-----------RESULTADO-----------"
+#define CORTE "-------------------------------------"
 
-int pedirCigarro();
-int pedirAnos();
-int calcularTempo(int cigarrosPorDia, int anosFumados);
-
-int main() {
-    printf("\n%s\n", inicio);
-    int cigarro, tempo, res;
-    cigarro = pedirCigarro();
-    tempo = pedirAnos();
-    res = calcularTempo(cigarro, tempo);
-    printf("\n%s\n", resultado);
-    puts("O tempo de vida estimado desse cidadao...\n");
-    printf("....E de aproximadamente: %d dias\n", res);
-    printf("\n%s\n", corte);
-    return 0;
-}
-
-int pedirCigarro() {
+int pedirCigarro(){
     int num;
-    printf("Digite a quantidade de cigarros fumados por dia: ");
-    scanf("%d", &num);
-    while (num <= 0) {
-        printf("Informação incompatível. Digite a quantidade de cigarros fumados por dia: ");
+    do{
+        printf("Digite a quantidade cigarros consumidos por dia: ");
         scanf("%d", &num);
-    }
+    } while ((num <= 0) || (num > 24));
     return num;
 }
 
-int pedirAnos() {
+int pedirAnos(){
     int num;
-    printf("Digite a quantidade de anos que essa pessoa fumou: ");
-    scanf("%d", &num);
-    while (num <= 0) {
-        printf("Informação incompatível. Digite a quantidade de anos que essa pessoa fumou: ");
+    do{
+        printf("Digite a quantidade de anos que esse individuo esta fumando: ");
         scanf("%d", &num);
-    }
+    } while ((num <= 0) || (num > 100));
     return num;
 }
 
-int calcularTempo(int cigarrosPorDia, int anosFumados) {
-    int minutosPerdidos = cigarrosPorDia * anosFumados * 365 * 10;  
-    int diasPerdidos = minutosPerdidos / 1440;  
+int calculo(int cigarro, int anos){
+    int minutosCigarro = 10, total, res, diasPerdidos;
+    total = cigarro * anos * 365;
+    res = total * minutosCigarro;
+    diasPerdidos = res / (24 * 60);
     return diasPerdidos;
+}
+int main(){
+    SetConsoleOutputCP(65001);
+    printf("\n%s\n", INICIO);
+    int cigarroDia, anos, res;
+    cigarroDia = pedirCigarro();
+    anos = pedirAnos();
+    res = calculo(cigarroDia,anos);
+    printf("\n%s\n", RESULTADO);
+    printf("Esse cidadão vai sobreviver %d dias a menos que o normal", res);
+    printf("\n%s\n", CORTE);
+    return 0;
 }
