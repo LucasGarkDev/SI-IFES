550a6ba32b30145d1cb3b862a1a21339e0808c20
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-09-17 19:09:50 -0300

    fdfdfd

--- SI_IFES/PROG_II/ex-presencial/lista1/lista 2/ex2_12.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista 2/ex2_12.c
@@ -12,28 +12,75 @@ nota.*/
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <math.h>
+#include <ctype.h>
 
-int main() {
-    float notas[5];
-    float maiorNota = 0, menorNota = 10, somaNotas = 0;
-    for (int i = 0; i < 5; i++) {
-        printf("Digite a nota do jurado %d: ", i + 1);
-        scanf("%f", &notas[i]);
-        // Encontrar a maior e a menor nota
-        if (notas[i] > maiorNota) {
-            maiorNota = notas[i];
-        }
-        if (notas[i] < menorNota) {
-            menorNota = notas[i];
-        }
-        somaNotas += notas[i];
+#define INICIO "--------INICIO--------"
+#define RESULTADO "-----------RESULTADO-----------"
+#define CORTE "-------------------------------------"
+
+float pedirNotas(int jurado){
+    float num;
+    do {
+        printf("Digite a nota do jurado %d: ", jurado);
+        scanf("%f", &num);
+    } while ((num < 0)||(num > 10));
+    return num;
+}
+
+void inserirNotas(float *notas, int quanti){
+    int i;
+    float nota;
+    for (i = 0; i < quanti; i++){
+        nota = pedirNotas(i+1);
+        notas[i] = nota;
     }
-    // Excluir a maior e a menor nota
-    somaNotas -= (maiorNota + menorNota);
-    // Calcular a média com as notas restantes
-    float media = somaNotas / 3;
-    printf("\nMédia das notas: %.2f\n", media);
-    printf("Maior nota: %.2f\n", maiorNota);
-    printf("Menor nota: %.2f\n", menorNota);
+}
+
+float encontrarMaiorMenor(float notas[], int quanti, int desci){
+    int i;
+    float maior = 0.0;
+    float menor = 99.0;
+    if (desci == 1){
+        for ( i = 0; i < quanti; i++){
+            if (notas[i] > maior){
+                maior = notas[i];
+            }
+        }
+        return maior;
+    }else{
+        for ( i = 0; i < quanti; i++){
+            if (notas[i] < menor){
+                menor = notas[i];
+            }
+        }
+        return menor;
+    }
+}
+
+float calMedia(float vetor[], float maior, float menor, int quanti){
+    int i;
+    float soma;
+    for (i = 0; i < quanti; i++){
+        soma += vetor[i];
+    }
+    return (soma - maior - menor)/ (quanti - 2);
+}
+
+int main(){
+    SetConsoleOutputCP(65001);
+    printf("\n%s\n", INICIO);
+    float notas[5];
+    float maior, menor, res;
+    inserirNotas(&notas, 5);
+    maior = encontrarMaiorMenor(notas,5,1);
+    menor = encontrarMaiorMenor(notas,5,2);
+    res = calMedia(notas,maior,menor,5);
+    printf("\n%s\n", RESULTADO);
+    printf("A media dessas notas foi: %.2f\n", res);
+    printf("A maior nota foi: %.2f\n", maior);
+    printf("A menor nota foi: %.2f", menor);
+    printf("\n%s\n", CORTE);
     return 0;
 }
