ef4464e8f5fffbce21cd78f5519c84e62f74c254
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-09-17 18:00:57 -0300

    vferfg

--- SI_IFES/PROG_II/ex-presencial/lista1/lista 2/ex2_1.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista 2/ex2_1.c
@@ -12,13 +12,35 @@ valor somado seja menor ou igual a 20, este deverá ser apresentado subtraindo-s
 #define RESULTADO "-----------RESULTADO-----------"
 #define CORTE "-------------------------------------"
 
+float pedirNum(int cont){
+    float num;
+    do{
+        printf("Digite o valor do %d numero: ", cont);
+        scanf("%f", &num);
+    } while ((num < 0)||(num > 10000));
+    return num;
+}
+
+float operacao(float numero1, float numero2){
+    float soma;
+    soma = numero1 + numero2;
+    if (soma > 20){
+        return soma + 8;
+    }else{
+        return soma - 5;
+    } 
+}
 
 int main(){
     SetConsoleOutputCP(65001);
     printf("\n%s\n", INICIO);
-    
+    float valor1, valor2, res;
+    valor1 = pedirNum(1);
+    valor2 = pedirNum(2);
+    res = operacao(valor1,valor2);
     printf("\n%s\n", RESULTADO);
-    printf("");
+    printf("Considerando a soma de %.2f e %.2f....\n", valor1, valor2);
+    printf(".... o valor apos a operaçao sera: %.2f", res);
     printf("\n%s\n", CORTE);
     return 0;
 }
