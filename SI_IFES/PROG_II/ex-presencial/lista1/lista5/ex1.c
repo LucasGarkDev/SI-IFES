8edc69fcef404b983c4902f41e3e4d09ba39a020
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-09-20 21:42:01 -0300

    hghjhg

--- SI_IFES/PROG_II/ex-presencial/lista1/lista5/ex1.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista5/ex1.c
@@ -3,35 +3,46 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <math.h>
+#include <ctype.h>
 
 #define INICIO "--------INICIO--------"
 #define RESULTADO "-----------RESULTADO-----------"
 #define CORTE "-------------------------------------"
-#define MAX 1000
+#define MAX 100
 
-void acrescenta(int vetor[],int quanti){
+// int pedirQuanti(int *vetor){
+//     int num;
+//     int tamanho = sizeof(vetor)/sizeof(vetor[0]);
+//     do{
+//         printf("Digite o tamanho do vetor: ");
+//         scanf("%d", &num);
+//     } while ((num <= 0)||(num > tamanho));
+//     return num;
+// }
+
+void preencherVetor(int *vetor, int quanti){
     int i;
-    int num = 100;
-    for (i = 0; i < quanti; i++){
-        vetor[i] = num;
-        num--;
+    for (i = quanti; i >= 0; i--){
+        vetor[i] = i;
     }
-    
 }
 
-void imprimir(int vetor[], int quanti){
+void imprimir(int *vetor, int quanti){
     int i;
     printf("\n%s\n", RESULTADO);
-    for (i = 0; i < 100; i++){
-        printf(" %d\n", vetor[i]);
+    for (i = 0; i < quanti; i++){
+        printf("%d,", vetor[i]);
     }
+    printf("\n%s\n", CORTE);
 }
 
-int main(){
-    printf("\n%s\n", INICIO);
+int main() {
+    SetConsoleOutputCP(65001);
     int vetor[MAX];
-    int quanti = 100;
-    acrescenta(vetor,quanti);
-    imprimir(vetor,quanti);
+    // int quanti = pedirQuanti(vetor);
+    preencherVetor(vetor,MAX);
+    imprimir(vetor,MAX);
     return 0;
 }
