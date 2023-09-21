551cf6f1a68f6927e43a3fbea74da1e22885fbd6
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-09-21 08:24:10 -0300

    kbklkj,

--- SI_IFES/PROG_II/ex-presencial/lista1/lista5/ex5.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista5/ex5.c
@@ -6,53 +6,64 @@ inserir e gerar outro número aleatório. No final imprima esse vetor.
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 #include <math.h>
+#include <ctype.h>
+#include <time.h>
 
 #define INICIO "--------INICIO--------"
 #define RESULTADO "-----------RESULTADO-----------"
 #define CORTE "-------------------------------------"
-#define MAX 1000
+#define MAX 100
 
-int aleatorio (int max) {
-    return ( rand() % max) + 1;
-}
-
-int pesquisar(int vetor[],int quanti,int pesq){
+int pesquisar(int *vetor, int quanti, int pesq){
     int i;
-    for(i = 0; i < quanti; i++){ 
-        if(vetor[i] == pesq){
+    for (i = 0; i < quanti; i++){
+        if (vetor[i] == pesq){
             return i;
-        }
+        } 
     }
     return -1;
 }
-
-void acrescentar(int vetor[],int quanti){
-    int i, num, verifica;
-    num = aleatorio(1000);
-    vetor[0] = num;
-    for (i = 1; i < quanti; i++){
-        do{
-            num = aleatorio(1000);
-            verifica = pesquisar(vetor,quanti,num);
-            vetor[i] = num;
-        } while (verifica != -1);
-    } 
+// int pedirQuanti(int *vetor){
+//     int num;
+//     int tamanho = sizeof(vetor)/sizeof(vetor[0]);
+//     do{
+//         printf("Digite o tamanho do vetor: ");
+//         scanf("%d", &num);
+//     } while ((num <= 0)||(num > tamanho));
+//     return num;
+// }
+int aletorio(int max) {
+    return (rand() % max) + 1;
 }
 
-void imprimir(int vetor[], int quanti){
-    int i;
+void preencherVetor(int *vetor, int quanti){
+    int i, verifica, guarda;
     for (i = 0; i < quanti; i++){
-        printf(" %d\n", vetor[i]);  
+        do{
+            guarda = aletorio(1000);
+            verifica = pesquisar(vetor,quanti,guarda);
+        } while (verifica != -1);
+        vetor[i] = guarda;
+    }
+}
+
+void imprimir(int *vetor, int quanti){
+    int i;
+    printf("\n%s\n", RESULTADO);
+    for (i = 0; i < quanti; i++){
+        printf("%d,", vetor[i]);
     }
     printf("\n%s\n", CORTE);
 }
 
-int main(){
-    printf("\n%s\n", RESULTADO);
+int main() {
+    SetConsoleOutputCP(65001);
+    srand((unsigned)time(NULL));
     int vetor[MAX];
-    int quanti = 100;
-    acrescentar(vetor,quanti);
-    imprimir(vetor,quanti);
+    // int quanti = pedirQuanti(vetor);
+    preencherVetor(vetor,MAX);
+    imprimir(vetor,MAX);
     return 0;
 }
