327c86befe3eee9427cfb1dba7cff6854a53518c
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-09-21 07:48:42 -0300

    ksdjk

--- SI_IFES/PROG_II/ex-presencial/lista1/lista5/ex2.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista5/ex2.c
@@ -4,45 +4,50 @@ Após isso, o programa deve imprimir todos os múltiplos de 5 armazenados nesse 
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <math.h>
+#include <ctype.h>
 
 #define INICIO "--------INICIO--------"
 #define RESULTADO "-----------RESULTADO-----------"
 #define CORTE "-------------------------------------"
-#define MAX 1000
+#define MAX 101
 
-void acrescenta(int vetor[],int quanti){
+// int pedirQuanti(int *vetor){
+//     int num;
+//     int tamanho = sizeof(vetor)/sizeof(vetor[0]);
+//     do{
+//         printf("Digite o tamanho do vetor: ");
+//         scanf("%d", &num);
+//     } while ((num <= 0)||(num > tamanho));
+//     return num;
+// }
+
+void preencherVetor(int *vetor, int quanti){
     int i;
     int num = 100;
-    for (i = 0; i < quanti; i++){
+    for (i = 0; i <= quanti; i++){
         vetor[i] = num;
         num++;
     }
-    
 }
 
-int multiplo5(int num){
-    if(num % 5 == 0){
-        return num;
-    }else{
-        return -1;
-    } 
-}
-
-void imprimir(int vetor[], int quanti){
-    int i, res;
+void imprimir(int *vetor, int quanti){
+    int i;
+    printf("\n%s\n", RESULTADO);
     for (i = 0; i < quanti; i++){
-        res = multiplo5(vetor[i]);
-        if (res != -1){
-            printf(" %d\n", res);
+        if (vetor[i] % 5 == 0){
+            printf("%d,", vetor[i]);
         }
     }
+    printf("\n%s\n", CORTE);
 }
 
-int main(){
-    printf("\n%s\n", INICIO);
+int main() {
+    SetConsoleOutputCP(65001);
     int vetor[MAX];
-    int quanti = 100;
-    acrescenta(vetor,quanti);
-    imprimir(vetor,quanti);
+    // int quanti = pedirQuanti(vetor);
+    preencherVetor(vetor,MAX);
+    imprimir(vetor,MAX);
     return 0;
 }
