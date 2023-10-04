4ae98e2b7c06b5e66f2cfa15393af79f786cff74
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-10-04 09:57:13 -0300

    snksdglox

--- SI_IFES/PROG_II/ex-presencial/lista1/lista7/ex2.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista7/ex2.c
@@ -1,3 +1,14 @@
+/*Fazer um programa que tenha duas matrizes 100x100 de números inteiros. 
+Faça uma função para preencher uma matriz com números aleatórios entre 1 e 
+100. Chame essa função duas vezes, uma para cada matriz. Em seguida ler um 
+inteiro m que servirá para multiplicar os elementos de cada uma das matrizes.
+Após fazer a multiplicação, o programa deve somar os elementos de mesma 
+posição de cada matriz e guardar em uma terceira matriz. No final, imprima 
+todas as três matrizes. É necessário apenas uma função imprimir. Chama-la 
+três vezes.  Seu programa precisará de cinco funções: preencher uma matriz 
+com números aleatórios, ler o inteiro m, multiplicar uma matriz por m, somar
+os elementos, imprimir uma matriz. */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
@@ -52,12 +63,23 @@ void imprimirMatriz(int matriz[][TAM]){
     }
 }
 
+void soma(int matriz1[][TAM],int matriz2[][TAM],int matriz3[][TAM]){
+    int i, j;
+    for (i = 0; i < TAM; i++){
+        for (j = 0; j < TAM; j++){
+            matriz3[i][j]= matriz1[i][j] + matriz2[i][j];
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     SetConsoleOutputCP(65001);
     // SEMENTE DOS NUMEROS ALEATÓRIOS. Usa a hora local
     srand((unsigned)time(NULL));
     int matriz1[TAM][TAM];
     int matriz2[TAM][TAM];
+    int matriz3[TAM][TAM];
     int multiNum;
     printf("\n%s\n", INICIO);
     lerMatriz(matriz1);
@@ -65,9 +87,13 @@ int main() {
     printf("Digite um valor valor para multiplicar as matrizes: ");
     scanf("%d", &multiNum);
     multiplicacao(matriz1,matriz2,multiNum);
+    soma(matriz1,matriz2,matriz3);
     printf("\n%s\n", RESULTADO);
-    // imprimirMatriz(matriz,1);
-    // printf("\n%s\n", CORTE);
-    // imprimirMatriz(matriz,0);
+    imprimirMatriz(matriz1);
+    printf("\n%s\n", CORTE);
+    imprimirMatriz(matriz2);
+    printf("\n%s\n", CORTE);
+    imprimirMatriz(matriz3);
+    printf("\n%s\n", CORTE);
     return 0;
 }
