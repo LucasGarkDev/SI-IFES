7790f2d299cb3464eb315a8ae6fba6bbe3282804
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-10-05 14:14:02 -0300

    Update ex4.c

--- SI_IFES/PROG_II/ex-presencial/lista1/lista8/ex4.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista8/ex4.c
@@ -14,6 +14,7 @@ e chame as funções para ler e mostrar na tela os dados desse polígono.
 #define INICIO "--------INICIO--------"
 #define RESULTADO "-----------RESULTADO-----------"
 #define CORTE "-------------------------------------"
+#define MAX 100
 
 struct Ponto{
     float x;
@@ -26,13 +27,18 @@ typedef struct Reta{
     Ponto pt1;
     Ponto pt2;
     char corReta[20];
-}Reta ;
+}Reta;
 
 typedef struct Circulo{
     Ponto centro;
     float raio;
     char corCirculo[20];
 }Circulo;
+
+struct Poligono{
+    Ponto vetor[100];    
+};
+typedef struct Poligono Poligono;
 
 float pedirXY(char letra, int indicador){
     float num;
@@ -43,52 +49,68 @@ float pedirXY(char letra, int indicador){
     return num;
 }
 
-void lerCorP(Ponto *cor2){
-    printf("Digite a cor do ponto: ");
-    scanf(" %19[^\n]s", cor2);
+int pedirQtde(){
+    int num;
+    do{
+        printf("Digite a quantidade de espaços no vetor: ");
+        scanf("%d", &num);
+    } while (num <= 0);
+    return num;
 }
 
-void lerCorR(Reta *cor2){
-    printf("Digite a cor da reta: ");
-    scanf(" %19[^\n]s", cor2);
+// void lerCorP(Ponto *cor2){
+//     printf("Digite a cor do ponto: ");
+//     scanf(" %19[^\n]s", cor2);
+// }
+
+// void lerCorR(Reta *cor2){
+//     printf("Digite a cor da reta: ");
+//     scanf(" %19[^\n]s", cor2);
+// }
+
+// void lerCorC(Circulo *cor2){
+//     printf("Digite a cor do circulo: ");
+//     scanf(" %19[^\n]s", cor2);
+// }
+
+void lerPonto(Ponto *vetor, int indicador){
+    vetor->x = pedirXY('X',indicador+1);
+    vetor->y = pedirXY('Y',indicador+1);
+    // lerCorP(p->cor);
 }
 
-void lerCorC(Circulo *cor2){
-    printf("Digite a cor do circulo: ");
-    scanf(" %19[^\n]s", cor2);
+// void lerReta(Reta *ret){
+//     lerPonto((&ret->pt1),1);
+//     lerPonto((&ret->pt2),2);
+//     lerCorR(&(ret->corReta));
+// }
+
+// void lerCirculo(Circulo *circ){
+//     printf("Digite o ponto em que o centro desse círculo está: \n");
+//     lerPonto(&(circ->centro), 1);
+//     printf("Digite o valor do raio desse círculo: ");
+//     scanf("%f", &(circ->raio)); 
+//     lerCorC(&(circ->corCirculo));
+// }
+
+void lerPoligono(Poligono *vet, int quanti){
+    int i;
+    for (i = 0; i < quanti; i++){
+        lerPonto(vet->vetor,i);
+    }
 }
 
-void lerPonto(Ponto *p, int indicador){
-    p->x = pedirXY('X',indicador);
-    p->y = pedirXY('Y',indicador);
-    lerCorP(p->cor);
-}
+// float calculoDist(Reta ret){
+//     return ((ret.pt2.x-ret.pt1.x)*(ret.pt2.x-ret.pt1.x)+(ret.pt2.y-ret.pt1.y)*(ret.pt2.y-ret.pt1.y))/2;
+// }
 
-void lerReta(Reta *ret){
-    lerPonto((&ret->pt1),1);
-    lerPonto((&ret->pt2),2);
-    lerCorR(&(ret->corReta));
-}
+// void calcularDistanciaReta(Reta ret, float *res){
+//     *res = calculoDist(ret);
+// }
 
-void lerCirculo(Circulo *circ){
-    printf("Digite o ponto em que o centro desse círculo está: \n");
-    lerPonto(&(circ->centro), 1);
-    printf("Digite o valor do raio desse círculo: ");
-    scanf("%f", &(circ->raio)); // Correção aqui
-    lerCorC(&(circ->corCirculo));
-}
-
-float calculoDist(Reta ret){
-    return ((ret.pt2.x-ret.pt1.x)*(ret.pt2.x-ret.pt1.x)+(ret.pt2.y-ret.pt1.y)*(ret.pt2.y-ret.pt1.y))/2;
-}
-
-void calcularDistanciaReta(Reta ret, float *res){
-    *res = calculoDist(ret);
-}
-
-void calCirculo(Circulo circ, float *res){
-    *res = M_PI * (circ.raio * circ.raio);
-}
+// void calCirculo(Circulo circ, float *res){
+//     *res = M_PI * (circ.raio * circ.raio);
+// }
 
 void imprimir(Circulo circ, float area){
     printf("\n%s\n", RESULTADO);
@@ -100,11 +122,14 @@ void imprimir(Circulo circ, float area){
 
 int main() {
     SetConsoleOutputCP(65001);
-    Circulo circ1;
-    float area;
+    Poligono vet[MAX];
+    int quanti = pedirQtde();
     printf("\n%s\n", INICIO);
-    lerCirculo(&circ1);
-    calCirculo(circ1,&area);
-    imprimir(circ1,area);
+    lerPoligono(vet,quanti);
+    int i;
+    for (i = 0; i < quanti; i++){
+        printf("%.2f\n", vet->vetor[i]);
+    }
+    
     return 0;
 }
