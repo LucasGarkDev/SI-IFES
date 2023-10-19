9f4e2de13674e6afeb6cbb55a88b5dacdafdddd9
Lucas Garcia <lucasgarciasouza12@gmail.com>
2023-10-19 10:43:29 -0300

    hbkdslkds

--- SI_IFES/PROG_II/ex-presencial/lista1/lista8/algo.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista8/algo.c
@@ -7,42 +7,52 @@
 #define RESULTADO "-----------RESULTADO-----------"
 #define CORTE "-------------------------------------"
 
-FILE * abrirArquivo(char * nomeArq, char * modo) {
-    FILE * arq;
-    arq = fopen( nomeArq, modo );
-    if ( arq == NULL) {
-        printf("Esse arquivo nao cosnta no diretorio.");
+FILE *abrirArquivo(char *nomeArq, char *modo) {
+    FILE *arq;
+    arq = fopen(nomeArq, modo);
+    if (arq == NULL) {
+        printf("Esse arquivo nao consta no diretorio.");
         exit(-1);
     }
     return arq;
 }
 
-void lerTexto(){
-    char texto[10000];
-    FILE * arq;
-    printf("Digite o texto que voce deseja: ");
-    fgets(texto,100,stdin);
-    fprintf(arq,"%s",texto);
-}
-
 int main() {
     SetConsoleOutputCP(65001);
-    FILE * arquivo;
+    FILE *arquivo;
     char palavra[20];
-    int cont;
-    char guarda[20];
+    int cont = 0;
+    char guarda[100]; // Aumentamos o tamanho para acomodar linhas inteiras
+    char texto[10000];
+    int compara;
+    char *token;
+
     arquivo = abrirArquivo("../arquivos/redacao.txt", "w");
-    lerTexto();
+    printf("Digite o texto que voce deseja: ");
+    fgets(texto, 100, stdin);
+    fprintf(arquivo, "%s", texto);
+    fclose(arquivo);
+
+    arquivo = abrirArquivo("../arquivos/redacao.txt", "r");
+
     printf("Digite a palavra que voce quer pesquisar: ");
-    fgets(palavra,20,stdin);
-    while (!feof(arquivo)){ 
-        fscanf(arquivo, " %19[^\n]s" , guarda);
-        if(strcmp(palavra, guarda) != feof(arquivo)){
-            cont++;
+    fgets(palavra, 20, stdin);
+
+    while (fgets(guarda, sizeof(guarda), arquivo) != NULL) {
+        token = strtok(guarda, " "); 
+        while (token != NULL) {
+            compara = strcmp(token, palavra);
+            if (compara == 0) {
+                cont++;
+            }
+            token = strtok(NULL, " "); 
         }
     }
-    printf("Foi contabilizado no : %d vezes que a palavra: %s apareçe no texto",cont,palavra);
+
+    printf("Foi contabilizado: %d vezes que a palavra %s aparece no texto\n", cont, palavra);
     fclose(arquivo);
-    printf("O processo foi executado com sucesso");    
+    printf("O processo foi executado com sucesso");
+
     return 0;
 }
+
