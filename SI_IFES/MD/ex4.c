9131e4acc56343d90e4279f8d49c908d3785baa0
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-11-01 11:05:11 -0300

    Create ex4.c

---
+++ SI_IFES/MD/ex4.c
@@ -0,0 +1,28 @@
+/*Escreva uma função recursiva que produza o reverso 
+de um string, reverse(char s[])*/
+#include <stdio.h>
+#include <string.h>
+
+#define INICIO "--------INICIO--------"
+#define RESULTADO "-----------RESULTADO-----------"
+#define CORTE "-------------------------------------"
+
+int string(char *s, int tam) {
+    if (tam == 0) {
+        return 0; 
+    } else {
+        return 1 + string(s + 1, tam - 1);
+    }
+}
+
+int main() {
+    int tam;
+    char str[50];
+    printf("\n%s\n", INICIO);
+    printf("Digite a frase: ");
+    fgets(str, sizeof(str), stdin);
+    tam = string(str, strlen(str) - 1); 
+    printf("\n%s\n", RESULTADO);
+    printf("O tamanho da string e: %d\n", tam);
+    return 0;
+}
