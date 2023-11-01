87e316c8fa30b927eb622454c5d371ae0bc9600b
Galaxyvideok <lucasgarciasouza12@gmail.com>
2023-11-01 14:25:14 -0300

    jbkjk

--- SI_IFES/PROG_II/ex-presencial/lista1/lista10/teste.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista10/teste.c
@@ -1,3 +1,8 @@
+/*Implemente a função recursiva para determinar o n-ésimo termo
+da sequência de Fibonnacci, onde os dois primeiros termos são 1
+e 1, e os demais são a soma dos dois termos anteriores
+1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -6,22 +11,23 @@
 #define RESULTADO "-----------RESULTADO-----------"
 #define CORTE "-------------------------------------"
 
-int fatorial(int base){
-    if (base == 0){
+int fibonaci(int n){
+    if (n == 1){
         return 1;
     } else {
-        return base*fatorial(base-1);
+        return (n-1)+fibonaci(n);
     }
 }
 
 int main(){
-    int n, e;
+    int e, n;
     printf("\n%s\n", INICIO);
-    printf("Base: ");
-    scanf("%d", &n);
-    e = fatorial(n);
+    // printf("Base: ");
+    // scanf("%d", &n);
+    n = 6;
+    e = fibonaci(n);
     printf("\n%s\n", RESULTADO);
-    printf("O fatorial de %d e: %d\n",n,e);
+    printf("O fibonaci de %d e: %d\n",n,e);
     printf("\n%s\n", CORTE);
     return 0;
 }
