ada8efeb7c8bec9b68ab39a2d08ddfa3afb0a6a9
Galaxyvideok <lucasgarciasouza12@gmail.com>
2023-11-01 13:07:27 -0300

    dsfdfssd

---
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista10/teste.c
@@ -0,0 +1,29 @@
+/*Escreva uma função recursiva para calcular o valor
+de uma base x elevada a um expoente y. */
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define INICIO "--------INICIO--------"
+#define RESULTADO "-----------RESULTADO-----------"
+#define CORTE "-------------------------------------"
+
+int potencia(int base, int expoente){
+    if (expoente == 0){
+        return 1;
+    } else {
+        return base * potencia(base, expoente - 1);
+    }
+}
+
+int main(){
+    int n, m, e;
+    printf("Base: ");
+    scanf("%d", &n);
+    printf("Expoente: ");
+    scanf("%d", &m);
+    e = potencia(n,m);
+    printf("\n%s\n", RESULTADO);
+    printf("%d elevado a %d e: %d\n",n,m,e);
+    return 0;
+}
