429b34e4c19287128aab000c62014d6690c192fb
Galaxyvideok <lucasgarciasouza12@gmail.com>
2023-11-01 13:58:45 -0300

    dsfdsdf

--- SI_IFES/MD/ex3.c
+++ SI_IFES/MD/ex3.c
@@ -7,21 +7,28 @@ caracter, caract(char c, char s[]) */
 #define RESULTADO "-----------RESULTADO-----------"
 #define CORTE "-------------------------------------"
 
-int string(char *s, int tam) {
-    if (tam == 0) {
-        return 0; 
-    } else {
-        return 1 + string(s + 1, tam - 1);
+int string(char *s, int tam,char letra) {
+    if (s[tam] == letra){
+        if (tam == 0) {
+            return 0; 
+        } else {
+            return 1 + string(s + 1, tam - 1, letra);
+        }
+    }else{
+        return 1 + string(s + 1, tam - 1, letra);
     }
 }
 
 int main() {
     int tam;
     char str[50];
+    char letra;
     printf("\n%s\n", INICIO);
     printf("Digite a frase: ");
     fgets(str, sizeof(str), stdin);
-    tam = string(str, strlen(str) - 1); 
+    printf("Digite o caracter que deseja procurar: ");
+    scanf("%c", &letra);
+    tam = string(str, strlen(str) - 1, letra); 
     printf("\n%s\n", RESULTADO);
     printf("O tamanho da string e: %d\n", tam);
     return 0;
