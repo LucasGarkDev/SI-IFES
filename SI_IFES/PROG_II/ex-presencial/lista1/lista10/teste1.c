379ba47e8986f8c16dbc2d7a9903d43884b20abb
Lucas Garcia <111055697+Galaxyvideok@users.noreply.github.com>
2023-11-06 08:52:50 -0300

    Create teste1.c

---
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista10/teste1.c
@@ -0,0 +1,32 @@
+/*Implemente a função recursiva para determinar o n-ésimo termo
+da sequência de Fibonnacci, onde os dois primeiros termos são 1
+e 1, e os demais são a soma dos dois termos anteriores
+1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, ...*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define INICIO "--------INICIO--------"
+#define RESULTADO "-----------RESULTADO-----------"
+#define CORTE "-------------------------------------"
+
+int fibonaci(int n){
+    if (n == 1|| n == 2){
+        return 1;
+    } else {
+        return fibonaci(n-1)+fibonaci(n-2);
+    }
+}
+
+int main(){
+    int e, n;
+    printf("\n%s\n", INICIO);
+    printf("Base: ");
+    scanf("%d", &n);
+    e = fibonaci(n);
+    printf("\n%s\n", RESULTADO);
+    printf("O fibonaci de %d e: %d\n",n,e);
+    printf("\n%s\n", CORTE);
+    return 0;
+}
