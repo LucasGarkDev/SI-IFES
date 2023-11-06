da0a2dc99108cb5431864691fc234f25ea929d15
Galaxyvideok <lucasgarciasouza12@gmail.com>
2023-11-06 18:04:51 -0300

    hkkf

--- SI_IFES/PROG_II/ex-presencial/lista1/lista10/teste3.c
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista10/teste3.c
@@ -1,5 +1,7 @@
-/*Implemente uma função recursiva para encontrar o maior
-elemento de um vetor de números inteiros*/
+/*Desafio: Crie uma função recursiva em C chamada contagem_de_modo que conte o número de 
+maneiras de representar um número n como uma soma de inteiros positivos, considerando 
+diferentes permutações. Por exemplo, para n = 4, existem cinco maneiras de representá-lo: 
+4, 3+1, 2+2, 2+1+1 e 1+1+1+1.*/
 
 #include <stdio.h>
 #include <stdlib.h>
