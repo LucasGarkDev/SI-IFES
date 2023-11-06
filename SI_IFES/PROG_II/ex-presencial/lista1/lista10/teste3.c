0b9dbdde951d6daed44b274ee84abbb02ec373c7
Galaxyvideok <lucasgarciasouza12@gmail.com>
2023-11-06 18:03:47 -0300

    hsdkjb

---
+++ SI_IFES/PROG_II/ex-presencial/lista1/lista10/teste3.c
@@ -0,0 +1,35 @@
+/*Implemente uma função recursiva para encontrar o maior
+elemento de um vetor de números inteiros*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define INICIO "--------INICIO--------"
+#define RESULTADO "-----------RESULTADO-----------"
+#define CORTE "-------------------------------------"
+
+int maiorVet(int *vet, int tam) {
+    int maior, resto;
+    if (tam == 1) {
+        return vet[0];
+    } else {
+        resto = maiorVet(vet, tam - 1);
+        if (vet[tam-1] > resto) {
+            return vet[tam - 1]; 
+        } else {
+            return resto; 
+        }
+    }
+}
+
+int main(){
+    int res,i = 5;
+    int vetor[5] = {9,76,43,12,23};
+    printf("\n%s\n", INICIO);
+    res = maiorVet(vetor, i);
+    printf("\n%s\n", RESULTADO);
+    printf("O maior do vetor e: %d\n",res);
+    printf("\n%s\n", CORTE);
+    return 0;
+}
